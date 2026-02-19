090a22564e0e3d1dc584e74f4c38370a05e9fd53
cikeseven <llyllydyx@163.com>
2026-02-20 00:55:02 +0800

    更新插件所使用的库

---
+++ libs/chaquopy/lib/cmake/openblas/OpenBLASConfig.cmake
@@ -0,0 +1,2 @@
+SET(OpenBLAS_VERSION "0.2.20")
+SET(OpenBLAS_INCLUDE_DIRS /home/smith/git/chaquo/python/server/pypi/packages/chaquopy-openblas/build/0.2.20/py3-none-android_21_arm64_v8a/prefix/chaquopy/include)
